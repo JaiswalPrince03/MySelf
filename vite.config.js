932841d1afc0bf445a6407c6e18bf8fe54ec48d5
JaiswalPrince03 <princejaiswal9199@gmail.com>
2024-10-22 22:17:16 +0530

    Fix BrowserRouter import and usage

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/JaiswalPrince03/MySelf.git',
+  base: "/MySelf/",
   plugins: [react()],
 })
